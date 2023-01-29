36f83f873b112c934e4bd2ce03d478ca2030cf9a
Homology1996 <karta2211845@gmail.com>
2023-01-29 22:42:42 +0800

    Create Depreciation.js

---
+++ NewYear/src/main/webapp/scripts/Depreciation.js
@@ -0,0 +1,11 @@
+function rate(New,Old,Life,Month){
+    return New-((New-Old)*Month)/Life
+}
+function price(Cost,Life,PurchaseTime,Start,End){
+    var New=1.5;
+    var Old=0.7;
+    var money=0;
+    for(var Month=0;Month<End-Start;Month++){
+
+    }
+}
